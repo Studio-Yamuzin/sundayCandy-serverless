031432bfc34d5c83368b6c3b97f1cc940e26115d
meohyun2 <eogus6242@gmail.com>
2021-09-06 10:48:06 +0900

    feat: preSignUp cognito trigger

--- src/auth/preSignUp.ts
+++ src/auth/preSignUp.ts
@@ -1,5 +1,6 @@
 import {Handler} from 'aws-lambda';
 export const main: Handler = async (event) => {
   event.response.autoConfirmUser = true;
+  event.response.autoVerifyEmail = true;
   return event;
 };
