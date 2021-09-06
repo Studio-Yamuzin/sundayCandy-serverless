d166072334c6e5fe98519fbc36462343a6e65dd2
meohyun2 <eogus6242@gmail.com>
2021-09-06 10:47:51 +0900

    feat: 카카오 signUp prototyping

--- src/auth/kakaoSignUp.ts
+++ src/auth/kakaoSignUp.ts
@@ -1,28 +1,29 @@
+import dynamodb from '@src/modules/dynamodb';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import {CognitoIdentityServiceProvider} from 'aws-sdk';
 import axios from 'axios';
 
 const cognitoServiceProvider = new CognitoIdentityServiceProvider({
-  apiVersion: '2016-04-10',
+  apiVersion: '2016-04-18',
 });
 const KAKAO_TOKEN_VERIFICATION_URL = "https://kapi.kakao.com/v2/user/me";
 
 export const main: APIGatewayProxyHandler = async (event, context) => {
-  const {kakaoAccessToken} = JSON.parse(event.body);
+  const {accessToken} = JSON.parse(event.body);
   const result = await axios.get(KAKAO_TOKEN_VERIFICATION_URL, {
     headers: {
-      Authorization: `Bearer ${kakaoAccessToken}`,
+      Authorization: `Bearer ${accessToken}`,
     }
   });
 
   const GroupName = 'kakao';
-  const UserPoolId = `{Cognito's User Pool id}`;  // aws-exports.js의 "aws_user_pools_id"
-  const ClientId = `{Cognito's Web client id}`; // aws-exports.js "aws_user_pools_web_client_id"
+  const UserPoolId = `ap-northeast-2_eOsy1c65x`;  // aws-exports.js의 "aws_user_pools_id"
+  const ClientId = `204t8opj0rek1k4qb5ca66d6cn`; // aws-exports.js "aws_user_pools_web_client_id"
   const Username = 'kakao_' + result.data.id;
   const newUserParam = {
     ClientId,
     Username,
-    Password: result.data.id.toString(),
+    Password: Username,
     ClientMetadata: {
       UserPoolId,
       Username,
@@ -40,13 +41,30 @@ export const main: APIGatewayProxyHandler = async (event, context) => {
     ],
   };
 
-  const cognitoSignUpResult = cognitoServiceProvider.signUp(newUserParam).promise();
+  const ddbParams = {
+    TableName: process.env.authTableName,
+    KeyConditionExpression: "PK = :Username",
+    ExpressionAttributeValues: {
+      ":Username": Username
+    }
+  };
+  
+  const existUser = await dynamodb.query(ddbParams);
+
+  if(existUser.Count === 0){
+    await cognitoServiceProvider.signUp(newUserParam).promise();
+    await dynamodb.put({
+      TableName: process.env.authTableName,
+      Item: {
+        PK: Username
+      }
+    });
+  }
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-        success: '카카오 인증 성공!',
-        body: cognitoSignUpResult,
-    }),
+      Username: Username
+    })
   }
 }
