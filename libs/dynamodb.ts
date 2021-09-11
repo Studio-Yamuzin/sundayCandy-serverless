de134dfd5b289692628099533294c7ac18a7ac86
meohyun2 <eogus6242@gmail.com>
2021-09-11 18:10:51 +0900

    feat: dynamodb libs

---
+++ libs/dynamodb.ts
@@ -0,0 +1,132 @@
+import {DynamoDB} from "aws-sdk";
+
+const client = new DynamoDB.DocumentClient();
+
+const call = (action, params) => {
+  let dynamoDb = new DynamoDB.DocumentClient({
+    convertEmptyValues: true,
+  });
+  if (process.env.stage == "local") {
+    dynamoDb = new DynamoDB.DocumentClient({
+      endpoint: process.env.dynamoLocalURL,
+      convertEmptyValues: true,
+    });
+  }
+  return dynamoDb[action](params).promise();
+};
+
+const queryByKeys = async(PK: string, SK: string) => {
+  const result = [];
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "PK = :PK and SK = :SK",
+    ExpressionAttributeValues: {
+      ":PK": PK,
+      ":SK": SK,
+    },
+    ScanIndexForward: false,
+    ExclusiveStartKey: "",
+  };
+  let items;
+  do {
+    items = await call("query", params);
+    items.Items.forEach(item => result.push(item));
+    params.ExclusiveStartKey = items.LastEvaluatedKey;
+  } while (typeof items.LastEvaluatedKey != "undefined");
+  return result;
+}
+
+const queryByPK = async(PK: string) => {
+  const result = [];
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "PK = :PK",
+    ExpressionAttributeValues: {
+      ":PK": PK,
+    },
+    ScanIndexForward: false,
+    ExclusiveStartKey: "",
+  };
+  let items;
+  do {
+    items = await call("query", params);
+    items.Items.forEach(item => result.push(item));
+    params.ExclusiveStartKey = items.LastEvaluatedKey;
+  } while (typeof items.LastEvaluatedKey != "undefined");
+  return result;
+}
+
+const queryBySK = async (SK: string) => {
+  const result = [];
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "SK = :SK",
+    ExpressionAttributeValues: {
+      ":SK": SK,
+    },
+    ScanIndexForward: false,
+    ExclusiveStartKey: "",
+  };
+  let items;
+  do {
+    items = await call("query", params);
+    items.Items.forEach(item => result.push(item));
+    params.ExclusiveStartKey = items.LastEvaluatedKey;
+  } while (typeof items.LastEvaluatedKey != "undefined");
+  return result;
+}
+
+
+const queryByBeginsWith = async(PK: string, word: string) => {
+  const result = [];
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "PK = :PK and begins_with(SK, :WORD)",
+    ExpressionAttributeValues: {
+      ":PK": PK,
+      ":WORD": word,
+    },
+    ScanIndexForward: false,
+    ExclusiveStartKey: "",
+  };
+  let items;
+  do {
+    items = await call("query", params);
+    items.Items.forEach(item => result.push(item));
+    params.ExclusiveStartKey = items.LastEvaluatedKey;
+  } while (typeof items.LastEvaluatedKey != "undefined");
+  return result;
+}
+
+const putItem = async (item) => {
+  try {
+    const params = {
+      TableName: process.env.tableName,
+      Item: item,
+    };
+
+    if(!item.createdAt) {
+      params.Item.createAt = new Date().toISOString();
+    }
+
+    if(!item.updatedAt) {
+      params.Item.updatedAt = new Date().toISOString();
+    }
+    await call("put", params);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
+
+export const dynamodb = {
+  putItem,
+  queryByKeys,
+  queryByPK,
+  queryBySK,
+  queryByBeginsWith,
+  get: (params) => client.get(params).promise(),
+  put: (params) => client.put(params).promise(),
+  query: (params) => client.query(params).promise(),
+  update: (params) => client.update(params).promise(),
+  delete: (params) => client.delete(params).promise(),
+};
