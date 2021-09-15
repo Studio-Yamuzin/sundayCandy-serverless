68af723c953036f12518c00bf0c0cccca9403cf6
meohyun2 <eogus6242@gmail.com>
2021-09-15 21:51:49 +0900

    feat: graphql handler, vtl

--- handlers/graphqlHandler.ts
+++ handlers/graphqlHandler.ts
@@ -2,18 +2,30 @@ import {Handler} from 'aws-lambda';
 
 import { createChurch } from '@src/church/createChurch';
 import { createProfile } from '@src/profile/createProfile';
+import { getUserChurch } from '@src/church/getUserChurch';
+import { getOnboardingStep } from '@src/onboarding/getOnboardingStep';
+import { getChurch } from '@src/church/getChurch';
+import { getProfile } from '@src/profile/getProfile';
 
 export const main: Handler = async(event, context) => {
+  console.log(JSON.stringify(context));
   console.log(`graphqlHandler =>\n{\nfield : '${event.field}',\nuserId : '${event.userId}'\n}`);
   console.log(event);
 
   switch(event.field){
     case "createChurch":
-      await createChurch(event.userId, event.arguments.input);
+      return await createChurch(event.userId, event.arguments.input);
+    case "getUserChurch":
+      return await getUserChurch(event.userId);
     case "getChurch":
+      return await getChurch(event.arguments.input.churchId);
     case "createProfile":
-      await createProfile(event.userId, event.arguments.input);
+      return await createProfile(event.userId, event.arguments.input);
+    case "getProfile":
+      return await getProfile(event.userId);
     case "getOnboardingStep":
-      await getOnboardingStep(event.userId);
+      return await getOnboardingStep(event.userId);
+    default:
+      return new Error("Wrong Field.");
   }
 }
