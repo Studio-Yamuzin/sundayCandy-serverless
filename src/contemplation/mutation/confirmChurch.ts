ee01a48959cbe9f2eb74bd5a36e29b7200fd4331
meohyun2 <eogus6242@gmail.com>
2021-12-27 22:57:56 +0900

    fix: 교회 승인 시 프리셋 게시판 로직 추가

--- src/contemplation/mutation/confirmChurch.ts
+++ src/contemplation/mutation/confirmChurch.ts
@@ -1,3 +1,4 @@
+import { BoardPresetType, CommunityType } from '@prisma/client';
 import { Church, ConfirmChurchInput } from '@src/types';
 import { prisma } from 'handlers/graphqlHandler';
 
@@ -13,6 +14,38 @@ export const confirmChurch = async ({
         isConfirmed: true,
       },
     });
+    if (updatedChurch.id) {
+      // 교회 홈 커뮤니티 생성
+      const createdCommunity = await prisma.community.create({
+        data: {
+          churchId: updatedChurch.id,
+          name: updatedChurch.name,
+          photo: updatedChurch.imageUri,
+          communityType: CommunityType.home,
+        },
+      });
+
+      // 교회 홈 주보 및 공지사항 게시판 생성
+      await prisma.board.createMany({
+        data: [
+          {
+            name: '공지사항',
+            boardPreset: BoardPresetType.list,
+            authority: 'admin',
+            communityId: createdCommunity.id,
+            order: 1,
+          },
+          {
+            name: '주보',
+            boardPreset: BoardPresetType.weeklyInfo,
+            authority: 'admin',
+            communityId: createdCommunity.id,
+            order: 2,
+          },
+        ],
+      });
+    }
+
     return {
       name: updatedChurch.name,
       description: updatedChurch.description,
