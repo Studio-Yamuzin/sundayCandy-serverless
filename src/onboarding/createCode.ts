7672f777a09e23667bc2f640dc13ec3fbdf50044
meohyun2 <eogus6242@gmail.com>
2021-12-27 22:59:23 +0900

    fix: createCode 반환 값 만료시간 추가

--- src/onboarding/createCode.ts
+++ src/onboarding/createCode.ts
@@ -1,5 +1,6 @@
 import { Code } from '@src/types';
 import { prisma } from 'handlers/graphqlHandler';
+import * as moment from 'moment';
 
 export const createCode = async (userId: string): Promise<Code> => {
   try {
@@ -13,13 +14,19 @@ export const createCode = async (userId: string): Promise<Code> => {
       data: {
         verifyNumber: randomNumber,
         churchId: userInfo.churchId,
+        makerId: userId,
       },
     });
+
+    const expiredTime = moment(createCodeResult.createdAt)
+      .add(1, 'hours')
+      .toISOString();
 
     return {
       id: createCodeResult.id,
       verifyNumber: createCodeResult.verifyNumber,
       createdAt: createCodeResult.createdAt.toUTCString(),
+      expiredAt: expiredTime,
     };
   } catch (error) {
     throw new Error('코드 발급에 실패하였습니다.');
